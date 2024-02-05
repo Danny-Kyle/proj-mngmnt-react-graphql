c0f339c736be03f0f3b483c45eca097040385393
Danny-Kyle <dikedaniel7917@gmail.com>
2024-02-05 13:26:53 +0100

    dotenv config

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
-const express = require ('express');
-require ('dotenv').config();
-const port = process.env.PORT || 3125
+const express = require("express");
+require("dotenv").config();
+const schema = require("./schema/schema");
+const { graphqlHTTP } = require("express-graphql");
+const port = process.env.PORT || 3125;
 
+const app = express();
 
-const app = express()
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
-app.listen(port, console.log("Running on port " + port))
+app.listen(port, console.log("Running on port " + port));
