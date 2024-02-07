33635043fdd1dbcc214a3d00c73263bd2f226a74
Danny-Kyle <dikedaniel7917@gmail.com>
2024-02-07 12:46:43 +0100

    mongoose conn establish

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const colors = require("colors");
 const schema = require("./schema/schema");
 const { graphqlHTTP } = require("express-graphql");
 const port = process.env.PORT || 3125;
