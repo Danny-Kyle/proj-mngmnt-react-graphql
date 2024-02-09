b277f1b742f4fde06654ea2a1af5eb34ce8b53ab
Danny-Kyle <dikedaniel7917@gmail.com>
2024-02-09 15:43:49 +0100

    retry this fix

--- server/models/Client.js
+++ server/models/Client.js
@@ -6,4 +6,4 @@ const ClientSchema = new mongoose.Schema({
   phone: { type: String },
 });
 
-module.exports = mongoose.model('Client', ClientSchema);
+module.exports = mongoose.model('Clients', ClientSchema);
