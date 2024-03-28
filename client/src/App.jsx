d22c4261e266d14757a97f57b32526a32baad2db
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-28 14:00:58 +0100

    change up rereloading upon deletion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 import Clients from "./components/Clients";
+import { Query } from "mongoose";
+
+const cache = new InMemoryCache({
+  typePolicies:{
+    Query:{
+      fields:{
+        clients:{
+          merge(existing, incoming){
+            return incoming
+          }, 
+        projects:{
+          merge(existing, incoming){
+            return incoming
+          }, 
+        },
+      }
+    }
+  }
+}})
 
 const client = new ApolloClient({
   uri: "http://localhost:3125/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App(){
