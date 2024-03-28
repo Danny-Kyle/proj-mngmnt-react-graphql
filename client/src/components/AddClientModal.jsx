7892b5dc42b61e75747c1a788443f85e7f441d0d
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-28 16:30:06 +0100

    edit modal

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -6,7 +6,10 @@ export default function AddClientModal() {
   return (
     <>
 <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
+  <div className="d-flex align-items-center">
+    <FaUser className='icon'/>
+    <span>Add Client</span>
+  </div>
 </button>
 
 <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -16,13 +19,28 @@ export default function AddClientModal() {
         <h1 className="modal-title fs-5" id="exampleModalLabel">Add New Client</h1>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
+
       <div className="modal-body">
-        ...
+        <form>
+            <div className="mb-3">
+                <label className="form-label">Full Name</label>
+                <input type="text" className="form-control" id='name'/>
+            </div>
+            <div className="mb-3">
+                <label className="form-label">Email Address</label>
+                <input type="text" className="form-control" id='email'/>
+            </div>
+            <div className="mb-3">
+                <label className="form-label">Phone Number</label>
+                <input type="text" className="form-control" id='phone'/>
+            </div>
+        </form>
       </div>
-      <div className="modal-footer">
+
+      {/* <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
+      </div> */}
     </div>
   </div>
 </div>
